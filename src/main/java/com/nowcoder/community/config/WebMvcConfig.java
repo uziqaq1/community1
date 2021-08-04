bb89971706a0886a712b3f4927d8d06e18e57c8e
wangyujun <867338858@qq.com>
2021-08-04 08:52:29 +0800

    7.5实现网站数据统计

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.config;
 
 import com.nowcoder.community.controller.MessageInterceptor;
 import com.nowcoder.community.controller.interceptor.AlphaInterceptor;
+import com.nowcoder.community.controller.interceptor.DataInterceptor;
 import com.nowcoder.community.controller.interceptor.LoginRequiredInterceptor;
 import com.nowcoder.community.controller.interceptor.LoginTicketInterceptor;
 import com.nowcoder.community.util.HostHolder;
@@ -19,11 +20,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
 
+
   /*  @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;*/
 
     @Autowired
     private MessageInterceptor messageInterceptor;
+
+    @Autowired
+    private DataInterceptor dataInterceptor;
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -34,6 +41,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 //       registry.addInterceptor(loginRequiredInterceptor)
         //      .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
         registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
+        registry.addInterceptor(dataInterceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
     }
 }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -9,28 +9,28 @@ public class RedisKeyUtil {
     private static final String PREFIX_FOLLOWER = "follower";
     private static final String PREFIX_KAPTCHA = "kaptcha";
     private static final String PREFIX_TICKET = "ticket";
-    private static final String PREFIX_USER= "user";
-
-
+    private static final String PREFIX_USER = "user";
+    private static final String PREFIX_UV = "uv";
+    private static final String PREFIX_DAO = "dao";
 
 
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
-    public static String getEntityLikeKey(int entityType,int entityId){
+    public static String getEntityLikeKey(int entityType, int entityId) {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
     }
 
     // 某个用户的赞
     // like:user:userId -> int
-    public static String getUserLikeKey(int userId){
-        return PREFIX_USER_LIKE+ SPLIT + userId;
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPLIT + userId;
     }
 
     // 某个用户关注的实体
     //followee:userId:entityType->zset(entityId,now)
-    public static String getFolloweeKey(int userId,int entityType){
+    public static String getFolloweeKey(int userId, int entityType) {
 
-        return PREFIX_FOLLOWEE+ SPLIT + userId+ SPLIT +entityType;
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
     }
 
     // 某个实体拥有的粉丝
@@ -40,17 +40,38 @@ public class RedisKeyUtil {
     }
 
     //登录验证码
-    public static String getKaptchaKey(String owner){
+    public static String getKaptchaKey(String owner) {
         return PREFIX_KAPTCHA + SPLIT + owner;
     }
 
     //登录凭证
-    public static String getTicketKey(String ticket){
+    public static String getTicketKey(String ticket) {
         return PREFIX_TICKET + SPLIT + ticket;
     }
 
     //用户
-    public static String getUserKey(int userId){
+    public static String getUserKey(int userId) {
         return PREFIX_USER + SPLIT + userId;
     }
+
+    //返回单日UV(Unique Visitor)
+    public static String getUVKey(String date) {
+        return PREFIX_UV + SPLIT + date;
+    }
+
+    //区间UV
+    public static String getUVKey(String startDate, String endDate) {
+        return PREFIX_UV + SPLIT + startDate + SPLIT + endDate;
+    }
+
+    //单日活跃用户(Daily Active User)
+    public static String getDAUKey(String date){
+        return PREFIX_DAO + SPLIT + date;
+    }
+
+    //区间活跃用户
+    public static String getDAUKey(String startDate, String endDate) {
+        return PREFIX_DAO+ SPLIT + startDate + SPLIT + endDate;
+    }
+
 }
