f4bb457a99ea15b98a1f4768b316c6c9fd9a9de4
wangyujun <867338858@qq.com>
2021-08-05 10:17:44 +0800

    7.7热帖排行

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -15,8 +15,8 @@ public class DiscussPostService {
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private SensitiveFilter sensitiveFilter;
-    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit){
-        return discussPostMapper.selectDiscussPosts(userId,offset,limit);
+    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit,int orderMode){
+        return discussPostMapper.selectDiscussPosts(userId,offset,limit,orderMode);
 
     }
 
@@ -59,4 +59,9 @@ public class DiscussPostService {
     public int updateStatus(int id,int status){
         return discussPostMapper.updateStatus(id,status);
     }
+
+    public int updateScore(int id,double score){
+        return discussPostMapper.updateScore(id,score);
+    }
+
 }
